e4a019b030e3f2b30f2ab2f0d91bbd2417942384
Laeeq <lladh918@mtroyal.ca>
2025-12-17 02:55:44 -0700

    so we can lock the screen but when we close the laptop it wont lock the screen. It also looks very ugly rightnow

---
+++ modules/home-manager/.config/sway/lock.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+swaylock \
+  --screenshots \
+  --effect-blur 10x5 \
+  --fade-in 0.3 \
+  --clock \
+  --indicator \
+  --ring-color 88c0d0 \
+  --key-hl-color a3be8c \
+  --inside-color 00000055 \
+  --text-color eceff4
+
