3116f378bb9906bc86e477c2a03b208c793ab9b3
Laeeq <lladh918@mtroyal.ca>
2026-08-28 07:06:34 -0600

    Update flake.lock and Niri configurations: refresh package versions, disable documentation builds, and adjust startup process for Noctalia.

--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -5,46 +5,49 @@
   system,
   ...
 }: {
-  # Enable Niri
+  # Disable problematic documentation builds
+  documentation = {
+    enable = false;
+    man.enable = false;
+    info.enable = false;
+    dev.enable = false;
+  };
+
   programs.niri = {
     enable = true;
     package = pkgs.niri;
-    # package = inputs.niri.packages.${pkgs.system}.niri;
   };
 
   environment.systemPackages = with pkgs; [
-  xwayland-satellite
-  alacritty
-  rofi
-  zsh
-  nodejs
-  ghostty 
-  typescript
-  pavucontrol
-  openjdk
-  pulseaudio
-  swaylock-effects
-  fprintd
-  grim
-  slurp 
-  jetbrains-toolbox
-  evince
-  python311
-  nmap
-  awscli
-  python3Packages.boto3
-  vim
-  openvpn
-  wireshark
-  (python3.withPackages(ps: with ps; [ numpy pandas ]))
+    xwayland-satellite
+    alacritty
+    rofi
+    zsh
+    nodejs
+    ghostty
+    typescript
+    pavucontrol
+    openjdk
+    pulseaudio
+    swaylock-effects
+    fprintd
+    grim
+    slurp
+    jetbrains-toolbox
+    evince
+    nmap
+    awscli
+    vim
+    openvpn
+    wireshark
 
-
-];
-
-
-  
-
-
+    # Python
+    (python312.withPackages (ps: with ps; [
+      numpy
+      pandas
+      boto3
+    ]))
+  ];
 
   services.displayManager.defaultSession = lib.mkForce "niri";
 }
